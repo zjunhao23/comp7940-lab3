702a16227019f38f9c9bd0fa3f6c63268ba3fb1f
zjh <zjunhao23@163.com>
2024-03-08 12:09:09 +0800

    Replace old file with new file

--- chatbot.py
+++ chatbot.py
@@ -1,22 +1,27 @@
 
+import os
+import telegram
 from telegram import Update
 from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters, 
                           CallbackContext)
-import configparser
+#import configparser
 import logging
-import redis
+#import redis
 from ChatGPT_HKBU import HKBU_ChatGPT
+import requests
+
 
 
 global redis1
 def main():
     # Load your token and create an Updater for your Bot
-    config = configparser.ConfigParser()
-    config.read('config.ini')
-    updater = Updater(token=(config['TELEGRAM']['ACCESS_TOKEN']), use_context=True)
+    #config = configparser.ConfigParser()
+    #config.read('config.ini')
+    #updater = Updater(token=(config['TELEGRAM']['ACCESS_TOKEN']), use_context=True)
+    updater = Updater(token=(os.environ['ACCESS_TOKEN']), use_context=True)
     dispatcher = updater.dispatcher
-    global redis1
-    redis1 = redis.Redis(host=(config['REDIS']['HOST']), password=(config['REDIS']['PASSWORD']), port=(config['REDIS']['REDISPORT']))
+    #global redis1
+    #redis1 = redis.Redis(host=(config['REDIS']['HOST']), password=(config['REDIS']['PASSWORD']), port=(config['REDIS']['REDISPORT']))
    
     # You can set this logging module, so you will know when and why things do not work as expected Meanwhile, update your config.ini as:
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
@@ -27,7 +32,8 @@ def main():
 
     # dispatcher for chatgpt
     global chatgpt
-    chatgpt = HKBU_ChatGPT(config)
+    #chatgpt = HKBU_ChatGPT(config)
+    chatgpt = HKBU_ChatGPT()
     chatgpt_handler = MessageHandler(Filters.text & (~Filters.command), equiped_chatgpt)
     dispatcher.add_handler(chatgpt_handler)
 
---
+++ readme.txt
@@ -0,0 +1,7 @@
+fly ips release 66.241.125.113 -a zjhchatbot-test
+chatbot ipv4地址：66.241.125.113
+ipv6: 2a09:8280:1::2d:2570:0
+
+C:\Users\zjunh\comp7940-lab3>fly ips allocate-v4 --shared -a zjhchatbot-test
+VERSION IP              TYPE    REGION
+v4      66.241.125.170  shared  global
